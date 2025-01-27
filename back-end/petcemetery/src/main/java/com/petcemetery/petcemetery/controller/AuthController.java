c01db0780a5da6e591c395b916dbd13590d46ceb
luizndh <79621478+luizndh@users.noreply.github.com>
2025-01-26 21:03:32 -0300

    retirando import nao utilizado

--- back-end/petcemetery/src/main/java/com/petcemetery/petcemetery/controller/AuthController.java
+++ back-end/petcemetery/src/main/java/com/petcemetery/petcemetery/controller/AuthController.java
@@ -3,9 +3,7 @@ package com.petcemetery.petcemetery.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
